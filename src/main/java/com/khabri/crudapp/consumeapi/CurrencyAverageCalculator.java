df0d087cba11bb9ea476e73a977ccb9889377680
Zafir Ahmad <ahmadzafir01@gmail.com>
2020-03-04 16:04:47 +0530

    Exchange class added

--- src/main/java/com/khabri/crudapp/consumeapi/CurrencyAverageCalculator.java
+++ src/main/java/com/khabri/crudapp/consumeapi/CurrencyAverageCalculator.java
@@ -1,19 +1,44 @@
 package com.khabri.crudapp.consumeapi;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/")
 public class CurrencyAverageCalculator {
 
+    private String url = "https://api.exchangeratesapi.io/";
+    private LocalDate localDate;
+    private String startDate = LocalDate.now().minusDays(15).toString();
+    private String endDate = LocalDate.now().toString();
+    private String urlToVisit =  url + startDate;
+    private Map<String,Double>map;
+
     @GetMapping("/")
     public String Home(){
         String home = "Index";
         return home;
+    }
+
+    @GetMapping("/")
+    public String CalculateAverageTill(@PathVariable(value = "endDate")String endDate){
+        this.endDate = endDate;
+        this.startDate = getLocalDate(endDate).minusDays(15).toString();
+        return "";
+    }
+
+    public String ConnectToInternet(){
+        return "";
+    }
+
+    private LocalDate getLocalDate(String date){
+        this.localDate = LocalDate.parse(date);
+        return localDate;
     }
 
 }
---
+++ src/main/java/com/khabri/crudapp/consumeapi/Exchange.java
@@ -0,0 +1,31 @@
+package com.khabri.crudapp.consumeapi;
+
+public class Exchange {
+    private String base;
+    private String date;
+    private Rates rates;
+
+    public String getBase() {
+        return base;
+    }
+
+    public void setBase(String base) {
+        this.base = base;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Rates getRates() {
+        return rates;
+    }
+
+    public void setRates(Rates rates) {
+        this.rates = rates;
+    }
+}
